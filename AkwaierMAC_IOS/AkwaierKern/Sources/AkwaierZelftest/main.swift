e9b363ebcac9ed7f9e15a77101c487c5a26197b0
Ed Nieuwenhuys <ednieuw@xs4all.nl>
2026-09-12 16:51:41 +0200

    V1.0.0

--- AkwaierMAC_IOS/AkwaierKern/Sources/AkwaierZelftest/main.swift
+++
@@ -1,9 +0,0 @@
-import AkwaierKern
-import Foundation
-
-// Draait alle proeven en drukt het verslag af. Sluit af met code 0 als alles
-// goed is, anders met het aantal mislukte controles.
-//     swift run AkwaierZelftest
-let uitslag = Zelftest.draai()
-print(uitslag.verslag)
-exit(uitslag.goed ? 0 : 1)
